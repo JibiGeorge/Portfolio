78462bf9e30d56e468dd2900c9a6caf66677e75d
Jibi George <jibiyyan@gmail.com>
2022-09-11 12:51:47 +0530

    Education Experience

--- js/script.js
+++ js/script.js
@@ -1,40 +1,42 @@
-	// Sticky Navbar
-    let header = document.querySelector('header');
-    let menu = document.querySelector('#menu-icon');
-    let navbar = document.querySelector('.navbar');
-     
-     
-    window.addEventListener('scroll', () => {
-        header.classList.toggle('shadow', window.scrollY > 0);
-    });
-     
-    menu.onclick = () => {
-        navbar.classList.toggle('active');
-    }
-    window.onscroll = () => {
-        navbar.classList.remove('active');
-    }
-     
-    // Dark Mode
-    let darkmode = document.querySelector('#darkmode');
-     
-    darkmode.onclick = () => {
-        if(darkmode.classList.contains('bx-moon')){
-            darkmode.classList.replace('bx-moon','bx-sun');
-            document.body.classList.add('active');
-        }else{
-            darkmode.classList.replace('bx-sun','bx-moon');
-            document.body.classList.remove('active');
-        }
-    }
+// Sticky Navbar
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxmZxFaHV7gZRjV4hgf2yymqp0TsqoQ3KlUh1yjF3XK-QBmbT5gy7k7IU4xzxSq12Tx/exec'
-        const form = document.forms['contactform']
-      
-        form.addEventListener('submit', e => {
-          e.preventDefault()
-          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => alert("Thank you! your form is submitted successfully." ))
-            .then(() => {  window.location.reload(); })
-            .catch(error => console.error('Error!', error.message))
-        })
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+// Dark Mode
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = () => {
+    if (darkmode.classList.contains('bx-moon')) {
+        darkmode.classList.replace('bx-moon', 'bx-sun');
+        document.body.classList.add('active');
+    } else {
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('active');
+    }
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxmZxFaHV7gZRjV4hgf2yymqp0TsqoQ3KlUh1yjF3XK-QBmbT5gy7k7IU4xzxSq12Tx/exec'
+const form = document.forms['contactform']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => alert("Thank you! your form is submitted successfully."))
+        .then(() => { window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
+})
+
